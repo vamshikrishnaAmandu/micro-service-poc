118eb3b9fff4e9f30baa4d090528b2abaac4aa52
krishna <krishna@gmail.com>
2020-08-11 22:59:57 +0530

    BeerController TestCases Implemented....

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("guru")
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
-    @GetMapping("{/beerId}")
+    @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
@@ -21,7 +21,7 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @PutMapping("{/beerId}")
+    @PutMapping("/{beerId}")
     public ResponseEntity updateBeer(@PathVariable UUID beerId,@RequestBody BeerDto beerDto){
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
